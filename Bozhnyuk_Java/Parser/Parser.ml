de8b69ebbfcf65a697817501ea8083b3be99f7da
bozhnyukAlex <bozhnyuks@mail.ru>
2020-11-11 12:01:58 +0300

    numeric, logic, testing expression parse implemented

--- Bozhnyuk_Java/Parser/Parser.ml
+++ Bozhnyuk_Java/Parser/Parser.ml
@@ -1,8 +1,7 @@
 open Opal
 open ATD
 
-let parens p = exactly ('(') >> lexeme p >>= fun x -> lexeme (exactly (')')) >> return x 
-
+let parens = between (token "(") (token ")")
 let not_space c = match c with
   | ' ' -> true
   | _ -> false
@@ -34,14 +33,67 @@ module Expr = struct
 
   let constInt = integer >>= fun n -> return (Const (JVInt n))
 
+  let reserved = [
+    "true";
+    "false";
+    "if";
+    "else";
+    "while";
+    "public";
+    "final";
+    "static";
+    "int";
+    "boolean";
+    "for";
+  ]
+  let ident = (spaces >> letter <~> many alpha_num) => implode >>= function
+  | s when List.mem s reserved -> mzero
+  | s -> return s
+
+  let addOp = token "+" >> return (fun x y -> NumericExpr(Add (x, y)))
+  let subOp = token "-" >> return (fun x y -> NumericExpr(Sub (x, y)))
+  let mulOp = token "*" >> return (fun x y -> NumericExpr(Mult (x, y)))
+  let divOp = token "/" >> return (fun x y -> NumericExpr(Div (x, y)))
+  let modOp = token "%" >> return (fun x y -> NumericExpr(Mod (x, y)))
+  let orOP = token "||" >> return (fun x y -> LogicalExpr(Or (x, y)))
+  let andOP = token "&&" >> return (fun x y -> LogicalExpr(And (x, y)))
+  let ltOp = token "<" >> return (fun x y -> TestingExpr (Less (x, y)))
+  let mtOp = token ">" >> return (fun x y -> TestingExpr (More (x, y)))
+  let loetOp = token "<=" >> return (fun x y -> TestingExpr (LessOrEqual (x, y)))
+  let moetOp = token ">=" >> return (fun x y -> TestingExpr (MoreOrEqual (x, y)))
+  let eqOp = token "==" >> return (fun x y -> TestingExpr (Equal (x, y)))
+  let neqOp = token "!=" >> return (fun x y -> TestingExpr (NotEqual (x, y)))
+
+
+
+  let atomaric = (ident => fun s -> Identifier s)
+              <|> (integer => fun n -> Const (JVInt n))
+              <|> (token "false" >> return (Const (JVBool true)))
+              <|> (token "true" >> return (Const (JVBool true)))
+              <|> (token "false" >> return (Const (JVBool false)))  
+
+  
+  
+
   let rec expression s = 
     choice [
       null;
       super;
       this;
+      numeric;
       
     ] s
-
+    and numeric input = (chainl1 and_expr orOP) input
+    and and_expr input = (chainl1 test_expr andOP) input
+    and test_expr input = (chainl1 add_expr (ltOp <|> mtOp <|> loetOp <|> moetOp <|> eqOp <|> neqOp)) input
+    and add_expr input = (chainl1 mult_expr (addOp <|> subOp)) input
+    and mult_expr input = (chainl1 unary_expr (mulOp <|> divOp <|> modOp)) input
+     and unary_expr input = choice [
+      (token "!" >> lexeme primary >>= fun s -> return (LogicalExpr (Not s)));
+      (token "-" >> lexeme primary >>= fun x -> return (NumericExpr (Sub (Const (JVInt 0), x))));
+      primary;
+    ] input 
+    and primary input = (parens expression <|> atomaric) input
     
 
   
